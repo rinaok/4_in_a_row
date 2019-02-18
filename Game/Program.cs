f9c448530639d011042d1230a2a65100bf63a1fe
rinaok <rinaok@gmail.com>
2019-02-18 19:21:57 +0200

    Add files via upload

---
+++ Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            GameSettingsD i_GameSettingForm = new GameSettingsD();
+            i_GameSettingForm.ShowDialog(); // open settings dialog
+        }
+    }
+}
